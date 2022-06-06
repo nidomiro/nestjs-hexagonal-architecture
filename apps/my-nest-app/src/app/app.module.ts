f89ff09a8d801841baacb408fe7f5561a3e640c6
Niclas Roßberger <niclas@nidomiro.de>
2022-06-06 21:53:50 +0200

    feat: moves repo impl and gql to adapters folder

--- apps/my-nest-app/src/app/app.module.ts
+++ apps/my-nest-app/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common'
-import { MyNestAppMonodbRepositoryModule } from '@nestjs-hexagonal-architecture/my-nest-app-monodb-repository'
+import { MyNestAppMonodbRepositoryModule } from '@nestjs-hexagonal-architecture/my-nest-app-adapters-monodb-repository'
 import {MyNestAppCoreModule, Repository} from '@nestjs-hexagonal-architecture/my-nest-app-core'
-import { MonodbRepositoryImpl } from '@nestjs-hexagonal-architecture/my-nest-app-monodb-repository'
+import { MonodbRepositoryImpl } from '@nestjs-hexagonal-architecture/my-nest-app-adapters-monodb-repository'
 import { GraphQLModule } from '@nestjs/graphql'
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
 import { join } from 'path'
-import { MyNestAppGraphqlModule } from '@nestjs-hexagonal-architecture/my-nest-app-graphql'
+import { MyNestAppGraphqlModule } from '@nestjs-hexagonal-architecture/my-nest-app-adapters-graphql'
 
 @Module({
 	imports: [
--- libs/my-nest-app/adapters/graphql/jest.config.ts
+++ libs/my-nest-app/adapters/graphql/jest.config.ts
@@ -1,7 +1,7 @@
 /* eslint-disable */
 export default {
   displayName: 'my-nest-app-graphql',
-  preset: '../../../jest.preset.js',
+  preset: '../../../../jest.preset.js',
   globals: {
     'ts-jest': {
       tsconfig: '<rootDir>/tsconfig.spec.json',
@@ -12,5 +12,5 @@ export default {
     '^.+\\.[tj]s$': 'ts-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../../coverage/libs/my-nest-app/graphql',
+  coverageDirectory: '../../../../coverage/libs/my-nest-app/graphql',
 };
